cfd5f93b003aaa90d0cd2938b58ceee1b0c5d54b
Jason Prince <jasoncprince@gmail.com>
2019-12-06 09:52:27 -0700

    Completed oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is the base class
+class Vehicle():
+    pass
+
+# Vehicle is the base class to FlightVehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# FlightVehicle is the base class to Starship
+class Starship(FlightVehicle):
+    pass
+
+# Vehicle is the base class to GroundVehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# FlightVehicle is the base class to Airplane
+class Airplane(FlightVehicle):
+    pass
+
+# GroundVehicle is the base class to Car
+class Car(GroundVehicle):
+    pass
+
+# GroundVehicle is the base class to Motorcycle
+class Motorcycle(GroundVehicle):
+    pass
